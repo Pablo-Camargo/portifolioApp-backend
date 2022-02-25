835ae682f80d9918b5a8985db5396211b4e2cb5e
Pablo-Camargo <pablo.m.c.elias@gmail.com>
2022-02-25 00:36:15 -0300

    node

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
+import express from 'express';
+import cors from 'cors';
+import routes from './router';
 
-const express = require('express');
-const cors = require('cors');
-
-
-
+import './database'
 const app = express();
 app.use(express.json());
 
@@ -12,8 +10,6 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
-app.use(require('./router'))
+app.use(routes)
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3001)
