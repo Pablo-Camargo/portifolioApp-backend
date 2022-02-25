9ec0fde96137990903181818ab2902f8a19b7845
Pablo-Camargo <pablo.m.c.elias@gmail.com>
2022-02-24 23:15:01 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -12,4 +12,6 @@ app.use(express.urlencoded({extended:false}));
 app.use(cors());
 app.use(routes)
 
-app.listen(process.env.PORT || 3001)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
